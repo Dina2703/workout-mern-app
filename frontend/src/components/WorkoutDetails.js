7964c6076c8851808d81920dd0d9449c6d3e4ec6
Dinara Idrissova <Dinara.Idrissova@list.ru>
2022-06-26 21:11:11 -0400

    fetch a single workoutdata & display it.

--- frontend/src/components/WorkoutDetails.js
+++ frontend/src/components/WorkoutDetails.js
@@ -1,18 +1,21 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function WorkoutDetails({ workout }) {
   return (
     <div className="workout-details">
-      <h4>{workout.title}</h4>
-      <p>
-        <strong>Load ()kg: </strong>
-        {workout.load}
-      </p>
-      <p>
-        <strong>Reps: </strong>
-        {workout.reps}
-      </p>
-      <p>{workout.createdAt}</p>
+      <Link to={`/${workout._id}`}>
+        <h4>{workout.title}</h4>
+        <p>
+          <strong>Load ()kg: </strong>
+          {workout.load}
+        </p>
+        <p>
+          <strong>Reps: </strong>
+          {workout.reps}
+        </p>
+        <p>{workout.createdAt}</p>
+      </Link>
     </div>
   );
 }
--- frontend/src/pages/WorkoutPage.js
+++ frontend/src/pages/WorkoutPage.js
@@ -1,7 +1,43 @@
-import React from "react";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 
 function WorkoutPage() {
-  return <div>WorkoutPage</div>;
+  const [workout, setWorkout] = useState(null);
+  const [load, setLoad] = useState(true);
+
+  const params = useParams();
+  useEffect(() => {
+    const fetchWorkout = async () => {
+      const response = await fetch(`/api/workouts/${params.id}`);
+      const data = await response.json();
+
+      if (response.ok) {
+        setWorkout(data);
+        setLoad(false);
+        console.log(data);
+      }
+    };
+
+    fetchWorkout();
+  }, [params]);
+
+  if (load) {
+    return <p>Loading</p>;
+  }
+  return (
+    <div className="workout-details">
+      <h4>{workout.title}</h4>
+      <p>
+        <strong>Load ()kg: </strong>
+        {workout.load}
+      </p>
+      <p>
+        <strong>Reps: </strong>
+        {workout.reps}
+      </p>
+      <p>{workout.createdAt}</p>
+    </div>
+  );
 }
 
 export default WorkoutPage;
